1061058ed7c846fe8e8be9fb6fb30f685f280d6d
Victoria Chen <54255894+victoriaychen@users.noreply.github.com>
2021-04-29 14:11:40 -0700

    updated server

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://vchen05:piano123@cluster0.1dyzc.mongodb.net/road_trip?retryWrites=true&w=majority";
 
 MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("road_trip");
+
+    dbo.collection("cities").find({}, {
+        projection: {
+            name: 1,
+            address: 1
+        }
+    }).toArray(function(err, result) {
         if (err) throw err;
-        var city = {
-            name: "San Diego",
-        };
-        db.collection("cities").insertOne(myobj, function(err, res) {
-            if (err) throw err;
-            console.log("1 record inserted");
-            db.close();
-        });
+        console.log(result);
+    });
+    // var myquery = {
+    //     address: "Valley 345"
+    // };
+    // var newvalues = {
+    //     $set: {
+    //         name: "Mickey",
+    //         address: "Canyon 123"
+    //     }
+    // };
+    //
+    // dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+    //     if (err) throw err;
+    //     console.log("1 record inserted");
+    //     db.close();
+    // });
 });
 
 $(function() {
-    $".save"
+    $ ".save"
 })
